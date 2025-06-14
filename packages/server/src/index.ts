61b0528418a6d967df58cfb54900fa8c84b63b7f
Conan Bitter <conan.bitter@gmail.com>
2025-06-14 15:16:40 +0300

    Try basic authentification

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,6 @@
-/*import { eq } from "drizzle-orm";
-import { db } from "./db/db";
-import * as schema from "./db/schema";
+/*
+
+
 
 const result = await db.select().from(schema.users);
 console.log(result);
@@ -17,9 +17,19 @@ if (admin.length > 0) {
 } else {
     console.log("Can't find admin");
 }*/
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { db } from "./db/db";
+import { eq } from "drizzle-orm";
+import * as schema from "./db/schema";
 
 const app = new Elysia()
+    .onError(({ code, error }) => {
+        if (code === 'VALIDATION') {
+            return {
+                error: error.all.map((item) => item.summary).join(". ")
+            };
+        }
+    })
     .get('/', ({ cookie: { auth } }) => {
         if (auth!.value) {
             return `Hello ${auth!.value}`;
@@ -27,6 +37,22 @@ const app = new Elysia()
             auth!.value = "User";
             return "Hello Elysia";
         }
+    })
+    .post('/login', async ({ body }) => {
+        const user = (await db.select().from(schema.users).where(eq(schema.users.login, body.login)))[0];
+        if (!user) {
+            return { "error": "Login or password is incorrect" };
+        }
+        if (await Bun.password.verify(body.password, user.password)) {
+            return { "error": "ok", "Name": user.name };
+        } else {
+            return { "error": "Login or password is incorrect" };
+        }
+    }, {
+        body: t.Object({
+            login: t.String(),
+            password: t.String()
+        }, { error: "Incorrect request" })
     })
     .listen(3000);
 
